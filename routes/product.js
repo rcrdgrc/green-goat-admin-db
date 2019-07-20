5c41be521e35ccb03bd09a27e64e030076558bb6
rcrdgrc <ricktx04@gmail.com>
2019-07-19 21:31:05 -0500

    database complete

--- routes/product.js
+++ routes/product.js
@@ -1,35 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { 
-    create, 
-    productById, 
-    read, 
-    remove, 
-    update, 
-    list, 
-    listRelated, 
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
     listCategories,
-    listBySearch, 
+    listBySearch,
     photo
-} = require('../controllers/product')
-    
-const { requireSignin, isAdmin, isAuth} = require('../controllers/auth')
-const { userById } = require('../controllers/user')
+} = require("../controllers/product");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-router.get('/product/:productId', read)
-router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
-router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove);
-router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update);
+router.get("/product/:productId", read);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
 
-router.get('/products', list)
-router.get('/products/related/:productId', listRelated)
-router.get('/products/categories', listCategories)
-router.post('/products/by/search', listBySearch)//we need post method to access req.body
-router.post('/product/photo/:productId', photo)
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
-
-router.param('userId', userById)
-router.param('productId', productById)
+router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
